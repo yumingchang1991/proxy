693d5efa41244460819628170a5f5678d3ab90cb
ChillDiver <yumingchang1991@gmail.com>
2022-08-19 16:32:37 +0800

    add 2 forms to AWS and Heroku respectively

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const codeExample = `{
+    status: "success",
+    message: "Your request reached AWS server with stock symbol: [YourSymbol]"
+  }`
   return (
     <div className="App">
-      <header className="App-header">
+      <main className="App-header">
+        <h2>This will sent to AWS with HTTP request</h2>
+        <p>HTTP request from HTTPS site is blocked by browser, so there will be warning.</p>
+        <p>If HTTP request sent successfully, AWS server returns JSON object as below:</p>
+        <code>{codeExample}</code>
         <form action="http://trackportfolio-env-1.eba-gswabkju.ap-southeast-1.elasticbeanstalk.com/eod/latest" method="POST">
           <div>
-            <label htmlFor="stock-code">Stock Symbol</label>
+            <label htmlFor="stock-symbol">Stock Symbol</label>
             <input id="stock-symbol" type='text' name='symbol' value='VTI'></input>
           </div>
           <button type="submit">get price</button>
         </form>
-      </header>
+      </main>
+      <main className="App-header">
+        <h2>This will sent to Heroku HTTPS Server</h2>
+        <p>the production code is based on Heroku server becasue of HTTPS</p>
+        <form action="https://etf-tracker.herokuapp.com/eod/latest" method="POST">
+          <div>
+            <label htmlFor="stock-symbol">Stock Symbol</label>
+            <input id="stock-symbol" type='text' name='symbol' value='VTI'></input>
+          </div>
+          <button type="submit">get price</button>
+        </form>
+      </main>
     </div>
   );
 }
